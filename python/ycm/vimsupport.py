6cef8750cc5e5c67fe01819ef820539c0cd56866
Evan Sebastian <evans@comp.nus.edu.sg>
2014-07-08 20:23:01 +0700

    Trying to fix #1069
    https://github.com/Valloric/YouCompleteMe/issues/1069
    
    To reproduce:
    1. Create any file with unicode character on its filename, e.g Ω.c
    2. Open the file and start adding codes until >5 lines (i.e ycm start compiling)
    3. YCM will crash due to unicode encoding error. See issue referenced above for complete log.
    
    Possible cause : On the function `GetBufferNumberForFilename` ,trying to format non-unicode string using unicode string
    Workaround : Convert the full path to utf-8 before passing to bufnr
    I have signed the Google CLA.

--- python/ycm/vimsupport.py
+++ python/ycm/vimsupport.py
@@ -103,8 +103,8 @@ def GetUnsavedAndCurrentBufferData():
 
 
 def GetBufferNumberForFilename( filename, open_file_if_needed = True ):
-  return GetIntValue( u"bufnr('{0}', {1})".format(
-      os.path.realpath( filename ),
+  return GetIntValue( "bufnr('{0}', {1})".format(
+      ToUtf8IfNeeded( os.path.realpath( filename ) ),
       int( open_file_if_needed ) ) )
 
 
